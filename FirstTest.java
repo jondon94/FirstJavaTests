21fd7a948e90055dd195b9e16590bb3eaf28ddc4
jondon94 <jondonaldson94@gmail.com>
2018-03-20 18:19:36 +0000

    test commit

--- FirstTest.java
+++ FirstTest.java
@@ -1,5 +1,6 @@
 //This a test file, here is where I will learn Java Syntax and how to run apps.
 // to run in Cmd, first compile using javac [filename.java], hit enter. then use java [filename]
+//test commit 
 public class FirstTest {
   public static void main(String[] args){
     System.out.println("Hello World");
